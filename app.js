3bd7b120c98f5b0a13ebcb76920a022fb07aba46
Pruthivi Thejan <pruthivithejan.code@gmail.com>
2022-10-13 11:55:03 +0530

    level4_bcrypt

--- app.js
+++ app.js
@@ -7,7 +7,11 @@ const { request } = require('http');
 const mongoose = require('mongoose');
 const { ifError } = require('assert');
 const { log } = require('console');
-const md5 = require('md5');
+
+//encryption constants
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
+
 
 const app = express();
 
@@ -46,37 +50,46 @@ app.get('/logout', (req, res) => {
 
 // create user
 app.post('/register', (req, res) => {
-    const newUser = new User({
-        email: req.body.username,
-       password: md5(req.body.password)    
-    });
-    
-    newUser.save(function (err, user){
-        if (err) {
-            console.log(err);
-        } else {
-            res.render("secrets");
-        }
-    }); 
+
+    bcrypt.hash(req.body.password, saltRounds,  function (err, hash){ 
+        const newUser = new User({
+            email: req.body.username,
+           password: hash   
+        });
+        
+        newUser.save(function (err, user){
+            if (err) {
+                console.log(err);
+            } else {
+                res.render("secrets");
+            }
+        }); 
+    });   
+
 });
 
 // check user for login
-app.post('/login', (req, res) => { 
-    const username = req.body.username;
-    const password = md5(req.body.password);
+app.post('/login', function(req, res) { 
 
-    User.findOne({email: username}, function(err, foundUser){
+    const username = req.body.username;
+    const password = req.body.password;
+
+    User.findOne({email: username}, function(err, foundUser) {
         if (err){
             console.log(err);
         } else {
             if (foundUser) {
-                if (foundUser.password === password) {
-                    res.render("secrets");
+                 bcrypt.compare(password, foundUser.password, function(err, result) {
+                    if (result === true) {
+                        res.render("secrets");
+                    }
+                 });   
                 }
             }   
-        }
+        });
     });
-});
+
+
 
 
 
